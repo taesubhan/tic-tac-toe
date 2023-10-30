a8753720738c8950f1f2215835926874c4ab5ac2
taesubhan <tshan499@gmail.com>
2023-10-29 21:46:23 -0700

    Complete game logic draft in javascript
    A draft of the game logic and DOM visualization of the game has been
    completed. No CSS has been applied so game is still bare bones.
    AI is yet to be implemented into the game as well.

--- javascript.js
+++ javascript.js
@@ -50,64 +50,13 @@ const gameBoard = (function() {
         return true;
     }
 
-    function isSamePlayer(array) {
-        const result =  array.every(item => {
-            return item !== null &&
-                    item.getPlayerValue() === array[0].getPlayerValue()});
-        return result;
-    }
-
-    function getWinner() {
-        for (let r = 0; r < rows; r++) {
-            if (isSamePlayer(board[r])) {
-                return board[r][0].getPlayerValue();
-            }
-        }
-
-        for (let c = 0; c < cols; c++) {
-            let column = [];
-            for (let r = 0; r < rows; r++) {
-                column.push(board[r][c]);
-            }
-            if (isSamePlayer(column)) {
-                return board[0][c].getPlayerValue();
-            }
-        }
-
-        let backward = [];
-        let forward = [];
-
-        for (let r = 0; r < rows; r++) {
-            
-            for (let c = 0; c < cols; c++) {
-                if (r === c) {
-                    backward.push(board[r][c]);
-                } else if ((r + c) === (rows - 1)) {
-                    forward.push(board[r][c])
-                }
-            }
-        }
-
-        if (isSamePlayer(backward)) {
-            return backward[0].getPlayerValue();
-        } else if (isSamePlayer(forward)) {
-            return forward[0].getPlayerValue();
-        }
-
-        return null;
-    }
-
-    function isDraw() {
-        return isFull() && !getWinner();
-    }
-
     function deleteBoard() {
         while (board.length > 0) {
             board.pop();
         }
     }
 
-    return {getBoard, createBoard, getCell, isCellTaken, isValidMove, placePlayerToken, getWinner, isDraw, isFull, deleteBoard};
+    return {getBoard, createBoard, getCell, isCellTaken, isValidMove, placePlayerToken, isFull, deleteBoard};
 })();
 
 // Game board cell factory function
@@ -156,12 +105,15 @@ const gameLogic = (function() {
     let playerA;
     let playerB;
     let currentPlayer;
-    const tokens = ['x','o']
 
-    function setUpGame(player1Name, player2Name) {
+    function getAllPlayersObject() {
+        return {playerA, playerB};
+    }
+    
+    function setUpGame(player1Name, player1Token, player2Name, player2Token) {
         gameBoard.createBoard();
-        playerA = player(player1Name, tokens[0]);
-        playerB = player(player2Name, tokens[1]);
+        playerA = player(player1Name, player1Token);
+        playerB = player(player2Name, player2Token);
         currentPlayer = playerA;
     }
 
@@ -182,28 +134,82 @@ const gameLogic = (function() {
     }
 
     function isGameOver() {
-        return gameBoard.isDraw() || Boolean(gameBoard.getWinner());
+        return isDraw() || Boolean(getWinner());
     }
 
     function getCurrentPlayer() {
         return currentPlayer.name;
     }
 
-    function startGame() {
-        setUpGame('Player 1', 'Player 2');
-    }
-
     function restartBackEnd() {
         gameBoard.deleteBoard();
-        startGame();
+        setUpGame(playerA.name, playerA.getToken(), playerB.name, playerB.getToken());
     }
 
-    return {startGame, playerInput, isGameOver, getCurrentPlayer, restartBackEnd};
+    function isSamePlayer(array) {
+        const result =  array.every(item => {
+            return item !== null &&
+                    item.getPlayerValue() === array[0].getPlayerValue()});
+        return result;
+    }
+
+    function getWinner() {
+        const board = gameBoard.getBoard();
+        const rows = board.length;
+        const cols = board[0].length;
+        
+        for (let r = 0; r < rows; r++) {
+            if (isSamePlayer(board[r])) {
+                return board[r][0].getPlayerValue();
+            }
+        }
+
+        for (let c = 0; c < cols; c++) {
+            let column = [];
+            for (let r = 0; r < rows; r++) {
+                column.push(board[r][c]);
+            }
+            if (isSamePlayer(column)) {
+                return board[0][c].getPlayerValue();
+            }
+        }
+
+        let backward = [];
+        let forward = [];
+
+        for (let r = 0; r < rows; r++) {
+            
+            for (let c = 0; c < cols; c++) {
+                if (r === c) {
+                    backward.push(board[r][c]);
+                } else if ((r + c) === (rows - 1)) {
+                    forward.push(board[r][c])
+                }
+            }
+        }
+
+        if (isSamePlayer(backward)) {
+            return backward[0].getPlayerValue();
+        } else if (isSamePlayer(forward)) {
+            return forward[0].getPlayerValue();
+        }
+
+        return null;
+    }
+
+    function isDraw() {
+        return gameBoard.isFull() && !getWinner();
+    }
+
+    return {getAllPlayersObject, setUpGame, playerInput, isGameOver, getCurrentPlayer, restartBackEnd, getWinner, isDraw};
 })();
 
 // DOM manipulating modules
 const gameInDOM = (function(doc) {
     let boardDOM = doc.querySelector('.game-board');
+
+    //dialog
+    const dialogBox =  doc.querySelector('#game-dialog');
 
     function drawBoard() {
         const board = gameBoard.getBoard();
@@ -220,14 +226,12 @@ const gameInDOM = (function(doc) {
                 boardDOM.appendChild(cellDOM);
             }
         }
-
-        addRestartFeature();
     }
 
     function displayWinner() {
-        if (gameBoard.getWinner()) {
-            alert(`Congratulations ${gameLogic.getCurrentPlayer()} won!`);
-        } else if (gameBoard.isDraw()) {
+        if (gameLogic.getWinner()) {
+            alert(`Congratulations, ${gameLogic.getCurrentPlayer()} wins!`);
+        } else if (gameLogic.isDraw()) {
             alert(`Draw!`)
         }
     }
@@ -268,18 +272,117 @@ const gameInDOM = (function(doc) {
             gameLogic.playerInput(row, col);
             e.target.textContent = gameBoard.getCell(row, col).getPlayerToken();
         }
-        setTimeout(() => gameLogic.isGameOver() 
-            ? endGame()  
-            : null
-            , 10); //used to allow textContent to be updated before alert function runs
+        setTimeout(() => {gameLogic.isGameOver() ? endGame() : null}, 10); 
+    }
+
+    function getCheckedValue (selectors) {
+        return Array.from(selectors).find((input) => input.checked).value;
+    }
+
+    function flipRadioValue(radio) {
+        Array.from(radio).find((button) => !button.checked).checked = true;
+    }
+
+    function preventSameToken(player1Radio, player2Radio) {
+        const allRadio = [player1Radio, player2Radio]
+
+        for (let radio of allRadio) {
+            radio.forEach((button) => {
+                button.addEventListener('change', (e) => { 
+                    radio === allRadio[0]
+                        ? flipRadioValue(allRadio[1])
+                        : flipRadioValue(allRadio[0]);
+                })
+            })
+        }
+    }
+
+    function removeStartButton() {
+        const startButton = doc.querySelector('.start-game');
+        if (startButton) startButton.hidden = true;
+    }
+
+    function addGameButtons() {
+        const restartButton = doc.querySelector('.restart-game');
+        const newGameButton = doc.querySelector('.new-game');
+        restartButton.hidden = false;
+        newGameButton.hidden = false;
+    }
+
+    function outputPlayerInfo() {
+        console.log(doc.querySelector('.player1-name.output'));
+        console.log(gameLogic.getAllPlayersObject().playerA.name);
+        console.log(gameLogic.getAllPlayersObject().playerB.name);
+        doc.querySelector('.player1-name.output').textContent = gameLogic.getAllPlayersObject().playerA.name;
+        doc.querySelector('.player1-token.output').textContent = gameLogic.getAllPlayersObject().playerA.getToken();
+        doc.querySelector('.player2-name.output').textContent = gameLogic.getAllPlayersObject().playerB.name;
+        doc.querySelector('.player2-token.output').textContent = gameLogic.getAllPlayersObject().playerB.getToken();
+
+    }
+
+    function setUpDialogBox() {
+        const player1Name = doc.querySelector('#player1Name');
+        const player2Name = doc.querySelector('#player2Name');
+        const player1Tokens = doc.querySelectorAll('input[name="player1Token"]');
+        const player2Tokens = doc.querySelectorAll('input[name="player2Token"]');
+        const form = doc.querySelector('.player-form');
+        const beginButton = doc.querySelector('.begin');
+        const cancelButton = doc.querySelector('.cancel');
+        dialogBox.returnValue = 'default';
+        
+        preventSameToken(player1Tokens, player2Tokens);
+        
+        beginButton.addEventListener('click', (e) => {
+            if (form.reportValidity()) {
+                e.preventDefault();
+                dialogBox.close('closed');
+            }
+        })
+
+        dialogBox.addEventListener('close', () => {
+            if (dialogBox.returnValue !== 'default') {
+                gameLogic.setUpGame(player1Name.value, getCheckedValue(player1Tokens), 
+                                        player2Name.value, getCheckedValue(player2Tokens));
+                deleteDOMBoard();
+                drawBoard();
+                removeStartButton();
+                addGameButtons();
+                outputPlayerInfo();
+            }
+            
+            form.reset(); // Clears input for next time dialog is opened
+        })
+
+        cancelButton.addEventListener('click', (e) => {
+            dialogBox.returnValue = 'default';
+            e.preventDefault();
+            dialogBox.close();
+        })
+    }
+
+    function addStartFeature() {
+        const startButton = doc.querySelector('.start-game');
+        startButton.addEventListener('click', () => dialogBox.showModal());
+    }
+
+    function addNewGameFeature() {
+        const newGameButton = doc.querySelector('.new-game');
+        newGameButton.addEventListener('click', () => {
+            
+            dialogBox.showModal();
+
+        })
+
     }
 
     function start() {
-        gameLogic.startGame();
-        drawBoard();
+        setUpDialogBox();
+        addStartFeature();
+        addRestartFeature();
+        addNewGameFeature();
     }
 
-    return {drawBoard, start};
+    return {start};
 })(document);
 
 gameInDOM.start();
